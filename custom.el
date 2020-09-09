cce0411058d1848d366751e7d2b0e4e303e62dc4
Zac Ioannidis <zacoppotamus@gmail.com>
2020-09-09 15:32:22 +0300

    Changed theme

---
+++ custom.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCDC"])
+ '(custom-safe-themes
+   '("a3b6a3708c6692674196266aad1cb19188a6da7b4f961e1369a68f06577afa16" "01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" "82360e5f96244ce8cc6e765eeebe7788c2c5f3aeb96c1a765629c5c7937c0b5b" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" default))
+ '(evil-goggles-mode t nil (evil-goggles))
+ '(evil-snipe-mode t)
+ '(fci-rule-color "#4F4F4F")
+ '(jdee-db-active-breakpoint-face-colors (cons "#000000" "#8CD0D3"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#7F9F7F"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#494949"))
+ '(minimap-mode t)
+ '(objed-cursor-color "#CC9393")
+ '(pdf-view-midnight-colors (cons "#DCDCDC" "#3F3F3F"))
+ '(rustic-ansi-faces
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCDC"])
+ '(vc-annotate-background "#3F3F3F")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#7F9F7F")
+    (cons 40 "#a4b48f")
+    (cons 60 "#cac99f")
+    (cons 80 "#F0DFAF")
+    (cons 100 "#eacfa4")
+    (cons 120 "#e4bf99")
+    (cons 140 "#DFAF8F")
+    (cons 160 "#dea3a0")
+    (cons 180 "#dd97b1")
+    (cons 200 "#DC8CC3")
+    (cons 220 "#d68eb3")
+    (cons 240 "#d190a3")
+    (cons 260 "#CC9393")
+    (cons 280 "#ab8080")
+    (cons 300 "#8a6d6d")
+    (cons 320 "#695b5b")
+    (cons 340 "#4F4F4F")
+    (cons 360 "#4F4F4F")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
