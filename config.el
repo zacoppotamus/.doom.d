846f35ddf1a73eaa9982d139d37ab50dfa1c558e
Zac Ioannidis <zacoppotamus@gmail.com>
2020-09-17 20:32:45 +0300

    Fix incorrect theme name

--- config.el
+++ config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'font-specdoom-one)
+(setq doom-theme 'doom-one)
 (setq doom-font (font-spec :family "Source Code Pro for Powerline" :size 13))
 
 ;; If you use `org' and don't want your org files in the default location below,
